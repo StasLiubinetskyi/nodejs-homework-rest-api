9786c51a3dfbb2564013d62d07de3765af1cd6be
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-10-23 15:48:32 +0300

    +unit-test

---
+++ __tests__/loginController.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../app");
+const mongoose = require("mongoose");
+
+const { DB_HOST } = process.env;
+
+describe("POST /users", () => {
+  beforeAll(() => {
+    mongoose.connect(DB_HOST).then(() => app.listen(3000));
+  });
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+
+  test("login", async () => {
+    const user = {
+      email: "user@example.com",
+      password: "password123",
+    };
+    const data = await request(app).post("/api/users/login").send(user);
+
+    expect(data.statusCode).toBe(200);
+    expect(data.body.token).toBeTruthy();
+    expect(data.body.user).toBeTruthy();
+    expect(typeof data.body.user.email).toContain("string");
+    expect(typeof data.body.user.subscription).toContain("string");
+  });
+});
